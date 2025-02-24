55d5d01c2c975543d5fcc256ba1cfa116f7269ed
zachey <63107653+zachey01@users.noreply.github.com>
2025-02-24 14:48:54 +0500

    Update getIcon.js

--- src/js/utils/getIcon.js
+++ src/js/utils/getIcon.js
@@ -1,9 +1,34 @@
+async function loadIcons() {
+    const url = 'https://www.unpkg.com/sigmaplayer/dist/sigma.svg';
+    const cacheKey = 'sigma-icons-cache';
+
+    const cachedSVG = localStorage.getItem(cacheKey);
+    if (cachedSVG) {
+        addSVGToHead(cachedSVG);
+    } else {
+        try {
+            const response = await fetch(url);
+            const svgContent = await response.text();
+            localStorage.setItem(cacheKey, svgContent);
+            addSVGToHead(svgContent);
+        } catch (error) {
+            console.error('Ошибка при загрузке SVG:', error);
+        }
+    }
+}
+
+function addSVGToHead(svgContent) {
+    const svgContainer = document.createElement('div');
+    svgContainer.innerHTML = svgContent;
+    const svg = svgContainer.querySelector('svg');
+    document.head.appendChild(svg);
+}
+
 function getIcon(iconId) {
     const svgElement = document.createElementNS(
         'http://www.w3.org/2000/svg',
         'svg',
     );
-
     svgElement.setAttribute('class', 'sigma__icon');
     svgElement.setAttribute('width', '24');
     svgElement.setAttribute('height', '24');
@@ -23,3 +48,5 @@ function getIcon(iconId) {
 
     return svgElement;
 }
+
+window.addEventListener('load', loadIcons);
